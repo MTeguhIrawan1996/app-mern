bac223ee38e3dcca44cda766f418dbee819762a7
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2022-06-28 22:23:16 +0700

    first commit

---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="main-page">
+      <div className="left">Ilustarasi</div>
+      <div className="right">Form Register</div>
+    </div>
+  );
+};
+
+export default Register;
