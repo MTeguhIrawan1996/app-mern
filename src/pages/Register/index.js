63125d505a49e62430c124ab02ab3d0f06989076
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2022-06-29 18:29:50 +0700

    form register dengan atom

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
 import "./register.scss";
 
 const Register = () => {
   return (
     <div className="main-page">
-      <div className="left">Ilustarasi</div>
-      <div className="right">Form Register</div>
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt="imageBg" />
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={30} />
+        <Link title="Kembali ke Login" />
+      </div>
     </div>
   );
 };
