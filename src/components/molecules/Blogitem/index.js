6d36fa86ca78dc122adc1734c7ba0b39c5a4a7cd
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2022-07-11 13:48:38 +0700

    Implamentasi Api

--- src/components/molecules/Blogitem/index.js
+++ src/components/molecules/Blogitem/index.js
@@ -1,33 +1,52 @@
 import React from "react";
-import { RegisterBg } from "../../../assets";
+// import { RegisterBg } from "../../../assets";
 import { Button } from "../../atoms";
 import "./blogitem.scss";
 import { useHistory } from "react-router-dom";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
   const history = useHistory();
+  const { image, title, name, date, body } = props;
   return (
-    <div className="blog-item">
-      <img className="image-thumb" src={RegisterBg} alt="post" />
-      <div className="content-detail">
-        <p className="title">Title Blog</p>
-        <p className="author">Author - Date Post</p>
-        <p className="body">
-          Excepteur aliqua esse veniam magna nisi nostrud fugiat ut elit culpa
-          ea. Labore minim sit tempor reprehenderit dolore eu nisi eu dolor.
-          Nisi cupidatat nisi magna cillum tempor sit. Amet commodo cupidatat ex
-          nostrud incididunt esse velit consectetur. Qui et cupidatat nisi
-          voluptate quis sit excepteur adipisicing. Elit cillum reprehenderit do
-          ex ea ipsum enim laboris ullamco ipsum. Excepteur id enim est fugiat
-          irure consequat sit anim quis officia elit.
-        </p>
-        <Button
-          title="View Detail"
-          className="button btn btn-success mt-2"
-          onClick={() => history.push("/detail-blog")}
-        />
+    <div className="col">
+      <div className="card mb-3">
+        <img src={image} className="card-img-top image-thumb" alt="post" />
+        <div className="card-body content-detail">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            <small className="text-muted">
+              {name} - {date}
+            </small>
+          </p>
+          <p className="card-text mt-2">{body}</p>
+        </div>
+        <div className="d-grid gap-2 col-6 mx-auto mb-3">
+          <Button
+            title="View Detail"
+            className="button btn btn-success mt-2"
+            onClick={() => history.push("/detail-blog")}
+          />
+        </div>
       </div>
     </div>
+
+    // {/* <div className="blog-item">
+    //   <img className="image-thumb" src={image} alt="post" />
+    //   <div className="content-detail">
+    //     <p className="title">{title}</p>
+    //     <p className="author">
+    //       {name} - {date}
+    //     </p>
+    //     <p className="body">{body}</p>
+    //   </div>
+    //   <div className="d-grid gap-2 col-6 mx-auto mb-3">
+    //     <Button
+    //       title="View Detail"
+    //       className="button btn btn-success mt-2"
+    //       onClick={() => history.push("/detail-blog")}
+    //     />
+    //   </div>
+    // </div> */}
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
+import Axios from "axios";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
+      .then((result) => {
+        console.log("data api", result.data);
+        const responAPI = result.data;
+
+        setDataBlog(responAPI.data);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+      });
+  }, []);
   const history = useHistory();
   return (
     <div className="home-page-wrapper">
@@ -16,10 +30,20 @@ const Home = () => {
       </div>
       <Gap height={20} />
       <div className="content-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          {dataBlog.map((blog) => {
+            return (
+              <BlogItem
+                key={blog._id}
+                image={`http://localhost:4000/${blog.image}`}
+                title={blog.title}
+                body={blog.body}
+                name={blog.author.name}
+                date={blog.createdAt}
+              />
+            );
+          })}
+        </div>
       </div>
       <div className="pagination">
         <Button title="Previous" className="button btn btn-success" />
